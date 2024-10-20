ddedf85f4531d8cfd6ee69ecec7be27b25f190fc
horrorbringer <vannysmilekh@gmail.com>
2024-10-20 15:32:34 +0700

    prepare navigation

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -3,15 +3,17 @@ const Login = () => {
   return (
     <>
       <article className="card-login">
-        <h2>Login</h2>
+        <h2 style={{ textAlign: "center" }}>Login</h2>
         <div className="list-input">
           <input type="text" name="" id="" placeholder="Username" />
           <input type="password" name="" id="" placeholder="Password" />
         </div>
-        <p>Forgetpassword</p>
-        <button type="button">Login</button>
-        <p>
-          Dont have an account<span>Register</span>
+        <p className='forget-pw'><a href="#">Forgetpassword</a></p>
+        <div style={{ textAlign: "center" }}>
+          <button type="button">Login</button>
+        </div>
+        <p className='log-regis'>
+          Dont have an account <a href='#'> Register</a>
         </p>
       </article>
     </>
