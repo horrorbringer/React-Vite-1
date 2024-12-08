55355e2cb822e6564e4d16b1bbb6950063327552
horrorbringer <vannysmilekh@gmail.com>
2024-12-08 15:31:52 +0700

    add link to my github

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 export const Footer = () => {
     return (
       <>
         <div className="line">
           <div className="line-footer"></div>
           <div className="icon">
-            <i className="fa-brands fa-github"></i>
+            <Link to="https://github.com/horrorbringer">
+              <i className="fa-brands fa-github"></i>
+            </Link>
             <i className="fa-brands fa-docker"></i>
             <i className="fa-brands fa-telegram"></i>
             <i className="fa-brands fa-facebook"></i>
